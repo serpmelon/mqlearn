3481d333c76b14979ec4b73a7100acaba39ba9c7
serpmelon <tyn243222791@126.com>
2019-09-24 11:32:47 +0800

    第三课

---
+++ consumer/src/main/java/com/togo/ReceiveLog.java
@@ -0,0 +1,70 @@
+package com.togo;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+/**
+ * <p></p>
+ * <p>
+ * <PRE>
+ * <BR>    修改记录
+ * <BR>-----------------------------------------------
+ * <BR>    修改日期         修改人          修改内容
+ * </PRE>
+ *
+ * @author taiyn
+ * @version 1.0
+ * @date Created in 2019年09月24日 11:13
+ * @since 1.0
+ */
+public class ReceiveLog {
+
+    private static final String QUEUE_NAME = "task_queue";
+    private static final String EXCHANGE = "logs";
+
+    public static void main(String[] args) throws Exception {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        int prefetchCount = 1;
+        // 一次只发送一个信息给消费者，消费者在处理完之前不回再被发送信息
+        channel.basicQos(prefetchCount);
+
+        channel.exchangeDeclare(EXCHANGE, "fanout");
+        String queueName = channel.queueDeclare().getQueue();
+        channel.queueBind(queueName, EXCHANGE, "");
+
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Received '" + message + "'");
+
+            try {
+                doWork(message);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println(" [x] Done");
+            }
+        };
+
+        boolean autoAck = false;
+        channel.basicConsume(queueName, autoAck, deliverCallback, consumerTag -> {});
+    }
+
+    private static void doWork(String task) throws InterruptedException {
+
+        for (char ch : task.toCharArray()) {
+
+            if (ch == '.')
+                Thread.sleep(1000);
+        }
+    }
+}
