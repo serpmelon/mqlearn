915ac65f7ed17266c2b5ab157aea51876162603e
serpmelon <tyn243222791@126.com>
2020-02-18 21:48:27 +0800

    rocket mq simple

--- rocket/src/main/java/com/togo/SimpleProducer.java
+++ rocket/src/main/java/com/togo/SimpleProducer.java
@@ -2,7 +2,9 @@ package com.togo;
 
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
 
 /**
  * @Author taiyn
@@ -13,10 +15,23 @@ public class SimpleProducer {
 
     public void syncSend() throws Exception {
 
-        DefaultMQProducer producer = new DefaultMQProducer();
+        DefaultMQProducer producer = new DefaultMQProducer("group_one");
         producer.setNamesrvAddr("localhost:9876");
         producer.start();
 
-        Message msg = new Message()
+        Message msg = new Message("topicTest" /* Topic */,
+                ("Hello RocketMQ ").getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
+        );
+
+        SendResult sendResult = producer.send(msg);
+        System.out.println(sendResult);
+
+        producer.shutdown();
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        SimpleProducer simpleProducer = new SimpleProducer();
+        simpleProducer.syncSend();
     }
 }
